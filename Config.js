abce3bfd3bc8af16f9cad5ca5e0de679ddd50cef
sirinapakhbudc-cyber <sirinapa.kh.budc@cpird.in.th>
2025-09-15 13:38:16 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,10 +1,11 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbw7mH3lrlvbe3slz6bLGcKk5yF4yKnzFw_p5bH8PkKJCQ0TUEkteqeK3Z2rZXROfp1x/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbyQ6aG3aHFUDgdxnCHGLl3xJQtsoWEreYMrqwRKp1Mq7A6dGxrLrwDLQV8d38TFJShM/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzqnv_BbtfDNXHJ4vf9tz47FuCfjEls2ddLD-LybBgoDwjybiB-LdGVRRZKiRFDjYfK/exec';
 //LIFF_ID
     const LIFF_ID = '2008088001-jO8Xk99z'; // LIFF ID Checkin
     const LIFF_HISTORY = '2008088001-v2OMLDDk'; // LIFF ID history
     const LIFF_REGISTER = '2008088001-kg4l9XXN'; // LIFF ID register
+
 
 
 
